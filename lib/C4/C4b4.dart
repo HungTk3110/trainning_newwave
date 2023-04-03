f0a36fc187d56b2fe4a151de90c306a390cd1b96
Sand3110 <trinhkhachung31102001@gmail.com>
2023-04-03 11:00:12 +0700

    c1+c3+c4

--- lib/C4/C4b4.dart
+++ lib/C4/C4b4.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class VideoApp extends StatefulWidget {
   const VideoApp({super.key});
@@ -11,6 +11,7 @@ class VideoApp extends StatefulWidget {
 
 class _VideoAppState extends State<VideoApp> {
   late VideoPlayerController _controller;
+  late YoutubePlayerController _ytbPlayerController;
 
   @override
   void initState() {
@@ -21,40 +22,72 @@ class _VideoAppState extends State<VideoApp> {
         // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
         setState(() {});
       });
+    _ytbPlayerController = YoutubePlayerController(
+      initialVideoId: YoutubePlayer.convertUrlToId(
+          "https://www.youtube.com/watch?v=YBRkVCRP1Gw") as String,
+      flags: YoutubePlayerFlags(
+        mute: false,
+        autoPlay: true,
+      ),
+    );
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Video Demo',
-      home: Scaffold(
-        body: Center(
-          child: _controller.value.isInitialized
-              ? AspectRatio(
-            aspectRatio: _controller.value.aspectRatio,
-            child: VideoPlayer(_controller),
-          )
-              : Container(),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              _controller.value.isPlaying
-                  ? _controller.pause()
-                  : _controller.play();
-            });
-          },
-          child: Icon(
-            _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        title: 'Video Demo',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Training Newwave C4 bai 4"),
           ),
-        ),
-      ),
-    );
+          body: Container(
+              padding: EdgeInsets.only(top: 20),
+              child: Column(
+                children: [
+                  Text("Sử dụng thư viện video_player"),
+                  Stack(
+                    children: [
+                      _controller.value.isInitialized
+                          ? AspectRatio(
+                              aspectRatio: _controller.value.aspectRatio,
+                              child: VideoPlayer(_controller),
+                            )
+                          : Container(),
+                      Positioned(
+                        left: 170,
+                        bottom: 80,
+                        child: FloatingActionButton(
+                          onPressed: () {
+                            setState(() {
+                              _controller.value.isPlaying
+                                  ? _controller.pause()
+                                  : _controller.play();
+                            });
+                          },
+                          child: Icon(
+                            _controller.value.isPlaying
+                                ? Icons.pause
+                                : Icons.play_arrow,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Text("Sử dụng thư viện youtube_player_flutter"),
+                  YoutubePlayer(
+                    controller: _ytbPlayerController!,
+                    showVideoProgressIndicator: true,
+                  ),
+                ],
+              )),
+        ));
   }
 
   @override
   void dispose() {
     super.dispose();
     _controller.dispose();
+    _ytbPlayerController.dispose();
   }
 }
